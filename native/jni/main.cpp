28fa2ab015ad8318b65958e6369f89dc53eee35c
Kẹo Kéo <kakathic@gmail.com>
2024-05-28 15:26:22 +0700

    Update main.cpp

--- native/jni/main.cpp
+++ native/jni/main.cpp
@@ -1,19 +1,6 @@
 #include <stdio.h>
 #include <iostream>
 
-#if defined(__arm__)
-#include "binaries/armeabi-v7a/busybox_bin.hpp"
-#elif defined(__aarch64__)
-#include "binaries/arm64-v8a/busybox_bin.hpp"
-#elif defined(__i386__)
-#include "binaries/x86/busybox_bin.hpp"
-#elif defined(__x86_64__)
-#include "binaries/x86_64/busybox_bin.hpp"
-#else
-#error Unsupported ABI
-#endif
-
-
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/mman.h>
@@ -30,23 +17,15 @@
 using namespace std;
 int main(int argc, char *argv[])
 {
-    const char *rawname = OBFUSCATE("busybox");
-    int bin_fd = memfd_create(rawname, 0);
-	if (bin_fd < 0 || ftruncate(bin_fd, busybox_bin.size) == -1)
-		return 1;
-	rawname = OBFUSCATE("script");
+    const char *rawname = OBFUSCATE("script");
 	int script_fd = memfd_create(rawname, 0);
 	if (script_fd < 0 || ftruncate(script_fd, strlen(shell_script)) == -1)
 		return 1;
 	
 	// Map the file into memory with read and write permissions
-	void *bin_addr = mmap(NULL, busybox_bin.size, PROT_READ | PROT_WRITE,
-                    MAP_SHARED, bin_fd, 0);
+	
  	// Copy the code into the mapped memory
- 	memcpy(bin_addr, busybox_bin.buf, busybox_bin.size);
-	// Change the memory protection to allow execution
-	mprotect(bin_addr, busybox_bin.size, PROT_READ | PROT_EXEC);
-
+ 	
 	
 	// Map the file into memory with read and write permissions
 	void *script_addr = mmap(NULL, strlen(shell_script), PROT_READ | PROT_WRITE,
@@ -69,7 +48,7 @@ int main(int argc, char *argv[])
 	    exec_argv[3+i] = strdup(argv[i]);
  	exec_argv[3+argc] = nullptr;
 	setenv(OBFUSCATE("ASH_STANDALONE"), OBFUSCATE("1"), true);
-	setenv(OBFUSCATE("BUSYBOX_PATH"), OBFUSCATE("/proc/self/exe"), true);
+	//setenv(OBFUSCATE("BUSYBOX_PATH"), OBFUSCATE("/proc/self/exe"), true);
 	setenv(OBFUSCATE("SCRIPT_PATH"), script_str, true);
 	setenv(OBFUSCATE("ORIGINAL_PATH"), argv[0], true);
 	xfexecve(bin_fd, exec_argv, environ);
